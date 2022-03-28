71a717d6e67f2cdfccd8090f5f022659d9592dd1
Richard K <kesslerrichardj@gmail.com>
2022-03-27 21:49:12 -0400

    port changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 // const bodyParser = require('body-parser');
 const axios = require('axios');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const dotenv = require('dotenv').config()
 
 
